fd824d5732681c60c80d6049be6783fe6f020405
RyanOrtiz0246 <ryan142310@gmail.com>
2025-02-07 17:03:18 -0600

    Creación de la clase base MiembroDeLaComunidad

---
+++ src/SalesDataAnalyticsPlatform/SalesDataAnalyticsPlatformApp/Models/MiembroDeLaComunidad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDataAnalyticsPlatformApp.Models
+{
+    public class MiembroDeLaComunidad
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public MiembroDeLaComunidad(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+        }
+    }
+}
