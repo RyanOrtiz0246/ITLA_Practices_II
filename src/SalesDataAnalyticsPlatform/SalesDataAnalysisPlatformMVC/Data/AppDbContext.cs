e8012a3f3bff3e976c12af855505fd9ec58ab9ab
RyanOrtiz0246 <ryan142310@gmail.com>
2025-02-08 19:22:30 -0600

    Configuración de la conexión a la base de datos y creación del DbContext

---
+++ src/SalesDataAnalyticsPlatform/SalesDataAnalysisPlatformMVC/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SalesDataAnalysisPlatformMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Sale> Sales { get; set; }
+    }
+}
