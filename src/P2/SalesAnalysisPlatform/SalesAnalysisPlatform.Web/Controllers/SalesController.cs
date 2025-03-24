5b055451100626cb07d0cc5f0bbf929ec799a6d3
RyanOrtiz0246 <ryan142310@gmail.com>
2025-03-23 18:07:22 -0600

    Se configuro el proyecto web

---
+++ src/P2/SalesAnalysisPlatform/SalesAnalysisPlatform.Web/Controllers/SalesController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesAnalysisPlatform.Application.Interfaces;
+using SalesAnalysisPlatform.Domain.Entities;
+
+namespace SalesAnalysisPlatform.Web.Controllers
+{
+    public class SalesController : Controller
+    {
+        private readonly ISaleService _saleService;
+
+        public SalesController(ISaleService saleService)
+        {
+            _saleService = saleService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var sales = await _saleService.GetAllSalesAsync();
+            return View(sales);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                await _saleService.AddSaleAsync(sale);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sale);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var sale = await _saleService.GetSaleByIdAsync(id);
+            if (sale == null) return NotFound();
+            return View(sale);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                await _saleService.UpdateSaleAsync(sale);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sale);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var sale = await _saleService.GetSaleByIdAsync(id);
+            if (sale == null) return NotFound();
+            return View(sale);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _saleService.DeleteSaleAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
