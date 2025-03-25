3ea9b064da8599461f8407a75af763c9a4bd93b5
RyanOrtiz0246 <ryan142310@gmail.com>
2025-03-24 20:40:47 -0600

    Se configuro el proyecto api

---
+++ src/P2/SalesAnalysisPlatform/SalesAnalysisPlatform.Api/Controllers/SalesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesAnalysisPlatform.Application.Interfaces;
+using SalesAnalysisPlatform.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SalesAnalysisPlatform.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        private readonly ISaleService _saleService;
+
+        public SalesController(ISaleService saleService)
+        {
+            _saleService = saleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sale>>> GetAllSales()
+        {
+            var sales = await _saleService.GetAllSalesAsync();
+            return Ok(sales);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sale>> GetSaleById(int id)
+        {
+            var sale = await _saleService.GetSaleByIdAsync(id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+            return Ok(sale);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateSale([FromBody] Sale sale)
+        {
+            if (sale == null)
+            {
+                return BadRequest();
+            }
+
+            await _saleService.AddSaleAsync(sale);
+            return CreatedAtAction(nameof(GetSaleById), new { id = sale.Id }, sale);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateSale(int id, [FromBody] Sale sale)
+        {
+            if (id != sale.Id)
+            {
+                return BadRequest();
+            }
+
+            await _saleService.UpdateSaleAsync(sale);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteSale(int id)
+        {
+            await _saleService.DeleteSaleAsync(id);
+            return NoContent();
+        }
+    }
+}
