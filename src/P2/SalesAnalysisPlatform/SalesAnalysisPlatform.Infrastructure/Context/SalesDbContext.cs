6ec51b79eda5f1701b3759c5efbde6463fa9f327
RyanOrtiz0246 <ryan142310@gmail.com>
2025-03-25 23:10:46 -0600

    Se implemento (reorganizo) capa Domain e Infrastructure siguiendo estructura de folders

--- src/P2/SalesAnalysisPlatform/SalesAnalysisPlatform.Infrastructure/Context/SalesDbContext.cs
+++ src/P2/SalesAnalysisPlatform/SalesAnalysisPlatform.Infrastructure/Context/SalesDbContext.cs
@@ -8,5 +8,16 @@ namespace SalesAnalysisPlatform.Infrastructure.Context
         public SalesDbContext(DbContextOptions<SalesDbContext> options) : base(options) { }
 
         public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sale>().ToTable("SALES", "RYAN_ORTIZ");
+
+            modelBuilder.Entity<Sale>().Property(s => s.Id).HasColumnName("ID");
+            modelBuilder.Entity<Sale>().Property(s => s.ProductName).HasColumnName("PRODUCTNAME");
+            modelBuilder.Entity<Sale>().Property(s => s.Price).HasColumnName("PRICE");
+            modelBuilder.Entity<Sale>().Property(s => s.Quantity).HasColumnName("QUANTITY");
+            modelBuilder.Entity<Sale>().Property(s => s.SaleDate).HasColumnName("SALEDATE");
+        }
     }
 }
