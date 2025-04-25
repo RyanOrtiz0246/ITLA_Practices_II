4861bb81437d7d03226ca36370e64ebd4ef52dff
RyanOrtiz0246 <ryan142310@gmail.com>
2025-04-24 19:34:09 -0600

    Se creo la nueva tabla en la base de datos con el nombre de Customers

--- src/P2/SalesAnalysisPlatform/SalesDB.sql
+++ src/P2/SalesAnalysisPlatform/SalesDB.sql
@@ -6,4 +6,15 @@ CREATE TABLE Sales (
     SaleDate DATE DEFAULT SYSDATE
 );
 
-SELECT * FROM Sales;
+CREATE TABLE Customers (
+    Id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Name VARCHAR2(255) NOT NULL,
+    Email VARCHAR2(100),
+    Phone VARCHAR2(20),
+    Address VARCHAR2(255)
+);
+
+INSERT INTO Customers (Name, Email, Phone, Address)
+VALUES ('Ryan Ortiz Campos', 'ryan1423@gmail.com', '809-123-4567', 'Santo Domingo Este');
+
+SELECT * FROM Customers;
